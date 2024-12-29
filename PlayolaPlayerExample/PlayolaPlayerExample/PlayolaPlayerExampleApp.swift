fb65e2a852ce646b24e60707b6a3719327afcdfb
Brian Keane <lonesomewhistle@gmail.com>
2024-12-29 11:28:14 -0600

    initial commit -- creation of package

---
+++ PlayolaPlayerExample/PlayolaPlayerExample/PlayolaPlayerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayolaPlayerExampleApp.swift
+//  PlayolaPlayerExample
+//
+//  Created by Brian D Keane on 12/29/24.
+//
+
+import SwiftUI
+
+@main
+struct PlayolaPlayerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlayolaPlayerExample/PlayolaPlayerExampleUITests/PlayolaPlayerExampleUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  PlayolaPlayerExampleUITestsLaunchTests.swift
+//  PlayolaPlayerExampleUITests
+//
+//  Created by Brian D Keane on 12/29/24.
+//
+
+import XCTest
+
+final class PlayolaPlayerExampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
