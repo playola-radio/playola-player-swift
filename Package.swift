d7d54b70969a1695cf09e2180be23c092e8a1e89
Brian Keane <lonesomewhistle@gmail.com>
2025-01-28 14:53:55 -0600

    remove mac

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "PlayolaPlayer",
     platforms: [
-      .iOS(.v17),
-      .macOS(.v15)
+      .iOS(.v17)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
