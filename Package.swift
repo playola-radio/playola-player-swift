910cb12c862cc18f7287c3af00e8947ced343485
Brian Keane <lonesomewhistle@gmail.com>
2025-04-09 16:32:18 -0500

    remove swiftlint plugin for now

--- Package.swift
+++ Package.swift
@@ -25,14 +25,14 @@ let package = Package(
           name: "PlayolaPlayer",
           resources: [.copy("MockData")],
             plugins: [
-              .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")
+//              .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")
             ]
         ),
         .testTarget(
             name: "PlayolaPlayerTests",
             dependencies: ["PlayolaPlayer"],
             plugins: [
-              .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")
+//              .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")
             ]
         )
     ]
