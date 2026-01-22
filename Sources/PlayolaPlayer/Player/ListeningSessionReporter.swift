bec61de3c3fe0c5ea9cd44b8bd0c02746dc03daa
Brian Keane <lonesomewhistle@gmail.com>
2026-01-21 20:27:24 -0600

    remove strong reference cycle to ListeningSessionReporter

--- Sources/PlayolaPlayer/Player/ListeningSessionReporter.swift
+++ Sources/PlayolaPlayer/Player/ListeningSessionReporter.swift
@@ -74,7 +74,8 @@ public class ListeningSessionReporter {
     self.urlSession = urlSession
     self.baseURL = baseURL
 
-    stationPlayer.$stationId.sink { stationId in
+    stationPlayer.$stationId.sink { [weak self] stationId in
+      guard let self else { return }
       if let stationId {
         Task {
           do {
